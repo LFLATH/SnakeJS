bf81efb151150aa83ca75556b4a5bcab623da7da
LFLATH <ludwiglechtreck@gmail.com>
2022-06-22 17:46:07 -0400

    Commit

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ const session = require('express-session');
 const flash = require("connect-flash");
 require('./config/passport')(passport); 
 
-const db = require('./config/keys').MongoURI;
 
-mongoose.connect(db, { useNewUrlParser: true })
-    .then(() => console.log("DB Connected"))
-    .catch(err => console.log(err))
-app.use(express.static(__dirname + '/views'));
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectToMongo = async() => {
+    await mongoose.connect(process.env.MongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    });
+    return mongoose;
+};
+
+await connectToMongo().then(async() => console.log('connected yeee'));
+
+
+
 
 app.use(
     session({
